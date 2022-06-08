552d05c4e5d132ef5298f6d832ff7054ce63c37d
rustiandacode <rustiandazen09@gmail.com>
2022-06-08 12:29:35 +0700

    membuat main views, navbar dan halaman home dan about

---
+++ resources/views/about.blade.php
@@ -0,0 +1,51 @@
+@extends('layouts.main')
+
+@section('container')
+
+<div class="container mt-4">
+    <div class="row justify-content-center">
+        <div class="col">
+            <div class="card bg-dark text-white">
+            <img src="https://source.unsplash.com/1200x400?dark" class="card-img" alt="...">
+                <div class="card-img-overlay">
+                <div class="row">
+                    <div class="col-md-6 p-5">
+                        <img src="https://source.unsplash.com/500x500?profile" width="100" alt="profile" class="img-thumbnail rounded-circle">
+                        <p class="card-text mt-2">
+                            <strong class="fs-5">Adzeni Rustianda</strong><br>
+                            <i class="fa-solid fa-envelope"></i> rustiandazen@gmail.com<br>
+                            <i class="fa-solid fa-map-pin"></i> Tangerang - Indonesia <br>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod optio repellat velit vitae, deserunt, ipsam sunt eaque.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="card text-center mt-3">
+    <div class="card-header">
+      Featured
+    </div>
+    <div class="card-body">
+      <h5 class="card-title">Special title treatment</h5>
+      <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+      <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
+    <div class="card-footer text-muted">
+      2 days ago
+    </div>
+</div>
+
+<div class="row mt-5">
+    <div class="col">
+        <h3 class="text-center">About</h3>
+        <hr>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero suscipit tempore veritatis officiis recusandae aut explicabo, inventore odit amet magnam cupiditate debitis neque quisquam labore consequuntur placeat quam optio, omnis fuga! Exercitationem, et accusantium. Ut ratione nemo modi recusandae cum ipsam sint quidem eum quod neque molestias delectus aliquid, maxime temporibus magni architecto vel saepe ducimus ea eveniet laborum accusantium! Culpa deserunt sequi vitae hic. Aliquam odit at consectetur, ipsa corrupti quasi, odio hic iusto aspernatur laboriosam rem suscipit dolorum unde! Repellat dolores illum animi, dolorem, nesciunt itaque placeat esse fugiat laboriosam corrupti asperiores sed omnis minus labore vero culpa.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus repudiandae, ullam at animi veniam veritatis illum, quasi reprehenderit facere dolor corrupti? Culpa maxime magni, corporis ex quos ut expedita ipsam quod, unde obcaecati accusantium praesentium dolorum eligendi sed ab distinctio repellat dicta laborum. Similique nisi voluptas maxime reprehenderit cupiditate aspernatur quia corporis tenetur, harum facere et possimus assumenda ab exercitationem? Quia distinctio quaerat officiis sit amet, accusantium tenetur quam numquam, mollitia sequi voluptas et dignissimos! Aperiam dignissimos obcaecati fugiat praesentium.</p>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Necessitatibus excepturi, sunt quis commodi perspiciatis, veniam, soluta eum aspernatur fugit voluptatem cum odio repellat voluptas recusandae officiis. Quis optio, saepe sint tempora ea tempore in eveniet accusamus nemo dolores neque reiciendis iure impedit quod facilis veritatis vitae beatae perferendis, perspiciatis omnis officiis. Veritatis, sint facilis ipsa ratione iure voluptatibus debitis. Dolorum, id, velit repellat enim maxime optio soluta hic qui dolore provident delectus incidunt laborum sed voluptates blanditiis tempora? Voluptate tempora soluta nihil! Recusandae, modi nisi iusto necessitatibus nobis beatae! Consequuntur accusamus ducimus sunt velit, tenetur nemo. Beatae quis voluptas sed, quam dolorum, facere quia accusantium excepturi, doloremque dolore numquam officiis! Atque quaerat veniam odio facere commodi quasi incidunt harum illo officia voluptatibus similique consequuntur distinctio error magnam quas, soluta odit laboriosam consectetur sit omnis! Eius, alias dignissimos quod quo ducimus pariatur perferendis? Delectus incidunt sapiente veritatis dolorum facilis illum a.</p>
+    </div>
+</div>
+
+  @endsection
---
+++ resources/views/home.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.main')
+@section('container')
+<div class="card mt-4">
+    {{-- <h5 class="card-header">Welcome to my website</h5> --}}
+    <div class="card-body">
+      <h5 class="card-header fs-3">Welcome to My Website!</h5>
+      <p class="card-text mt-2">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eligendi mollitia aliquid explicabo velit, aspernatur sed aperiam doloremque saepe porro, qui enim beatae doloribus id aliquam! Dolorum expedita asperiores rerum nihil saepe sequi placeat, quos repudiandae sint, nesciunt minus officia reprehenderit magni repellendus dolores alias natus quis voluptas cumque? Alias illo obcaecati libero numquam voluptatum fuga veritatis, voluptatibus deserunt nobis facilis, animi cum veniam a facere mollitia ducimus ea pariatur molestias consequatur corporis ullam eum qui expedita? Temporibus doloremque laudantium minima vel culpa! Accusantium, dolorem officia voluptatum, maiores nisi quas inventore minus iure distinctio et in minima voluptatem non impedit quos?</p>
+    </div>
+  </div>
+@endsection
